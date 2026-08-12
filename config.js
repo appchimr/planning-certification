76b53e992a2c18a7bf49319083d90f1b474060e0
chimr <communication.chimr@gmail.com>
2026-08-12 21:02:08 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwL85Tq-9hBIcCBJZHF3xFl0WuXXZFJF4FcdHdXjE0XAVKa5LI0a9kNc-I0FAf6CB0Z/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzfL33Jk1td-xzc7gqQ0F2wb167yXj1cDkMYW1cHW7x4NRdvhihOE9K1BLZ8FUmc5jB/exec"
 };
